20c6562502c47351d4d08e357a059dbb7b16def6
Claire Roche <claireroche47@gmail.com>
2026-05-06 16:13:44 +0200

    add bezier curve tests

--- src/bezier_curve.py
+++ src/bezier_curve.py
@@ -6,14 +6,8 @@ def binomial(n,i):
 def bernstein(i,n,t):
     return binomial(n,i)*pow(t,i)*pow(1.0-t,n-i)
 
-def bezier(pts, t):
-    sum = 0
-    for i in range(0,len(pts)):
-        sum = sum + bernstein(i,len(pts)-1,t)*pts[i]
-    return sum
-
-assert(binomial(10,10)==1)
-assert(binomial(10,5)==252)
-
-#print(bezier([0,0.3,0.7,1], 0.5))
-
+def bezier_curve(ctrl_pts, t):
+    bc = 0
+    for i in range(0, len(ctrl_pts)):
+        bc = bc + bernstein(i, len(ctrl_pts) - 1, t) * ctrl_pts[i]
+    return bc
---
+++ tst/test_bezier_curve.py
@@ -0,0 +1,5 @@
+from src.bezier_curve import *
+
+def test_binomial():
+    assert(binomial(10,10)==1)
+    assert(binomial(10,5)==252)
