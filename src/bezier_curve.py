8f501236394181a669c03504ecd0b32e203917b8
Claire Roche <claireroche47@gmail.com>
2026-05-07 10:41:19 +0200

    update bezier curves to handle 3D points

--- src/bezier_curve.py
+++ src/bezier_curve.py
@@ -7,7 +7,12 @@ def bernstein(i,n,t):
     return binomial(n,i)*pow(t,i)*pow(1.0-t,n-i)
 
 def bezier_curve(ctrl_pts, t):
-    bc = 0
+    bc_x = 0
+    bc_y = 0
+    bc_z = 0
     for i in range(0, len(ctrl_pts)):
-        bc = bc + bernstein(i, len(ctrl_pts) - 1, t) * ctrl_pts[i]
-    return bc
+        b = bernstein(i, len(ctrl_pts) - 1, t)
+        bc_x = bc_x + b * ctrl_pts[i][0]
+        bc_y = bc_y + b * ctrl_pts[i][1]
+        bc_z = bc_z + b * ctrl_pts[i][2]
+    return (bc_x, bc_y, bc_z)
