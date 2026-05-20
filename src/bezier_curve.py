a1b07e5af55ee69e99df6c73ce9942c4d9a07f41
Claire Roche <claireroche47@gmail.com>
2026-05-20 15:47:11 +0200

    add bezier curve subdivision and unit tests

--- src/bezier_curve.py
+++ src/bezier_curve.py
@@ -6,6 +6,17 @@ def binomial(n,i):
 def bernstein(i,n,t):
     return binomial(n,i)*pow(t,i)*pow(1.0-t,n-i)
 
+def de_casteljau(ctrl_pts, i, j, t):
+    if j == 0:
+        return ctrl_pts[i]
+    else:
+        pi   = de_casteljau(ctrl_pts,   i, j-1, t)
+        pipu = de_casteljau(ctrl_pts, i+1, j-1, t)
+        p_x = (1.0-t)*pi[0] + t*pipu[0]
+        p_y = (1.0-t)*pi[1] + t*pipu[1]
+        p_z = (1.0-t)*pi[2] + t*pipu[2]
+        return (p_x, p_y, p_z)
+
 def bezier_curve(ctrl_pts, t):
     bc_x = 0
     bc_y = 0
@@ -16,3 +27,12 @@ def bezier_curve(ctrl_pts, t):
         bc_y = bc_y + b * ctrl_pts[i][1]
         bc_z = bc_z + b * ctrl_pts[i][2]
     return (bc_x, bc_y, bc_z)
+
+def bezier_curve_subdivision(ctrl_pts, t0):
+    ctrl_pts_1 = []
+    ctrl_pts_2 = []
+    n = len(ctrl_pts)-1
+    for i in range(0, len(ctrl_pts)):
+        ctrl_pts_1.append(de_casteljau(ctrl_pts, 0, i, t0))
+        ctrl_pts_2.append(de_casteljau(ctrl_pts, i, n-i, t0))
+    return ctrl_pts_1, ctrl_pts_2
--- tst/test_bezier_curve.py
+++ tst/test_bezier_curve.py
@@ -1,79 +1,121 @@
-import src.bezier_curve as spleen_bezier_curve
+import src.bezier_curve as spleen_bc
 
 def test_binomial():
 
-    assert(spleen_bezier_curve.binomial(1, 0) == 1)
-    assert(spleen_bezier_curve.binomial(1, 1) == 1)
+    assert(spleen_bc.binomial(1, 0) == 1)
+    assert(spleen_bc.binomial(1, 1) == 1)
 
-    assert(spleen_bezier_curve.binomial(2, 0) == 1)
-    assert(spleen_bezier_curve.binomial(2, 1) == 2)
-    assert(spleen_bezier_curve.binomial(2, 2) == 1)
+    assert(spleen_bc.binomial(2, 0) == 1)
+    assert(spleen_bc.binomial(2, 1) == 2)
+    assert(spleen_bc.binomial(2, 2) == 1)
 
-    assert(spleen_bezier_curve.binomial(3, 0) == 1)
-    assert(spleen_bezier_curve.binomial(3, 1) == 3)
-    assert(spleen_bezier_curve.binomial(3, 2) == 3)
-    assert(spleen_bezier_curve.binomial(3, 3) == 1)
+    assert(spleen_bc.binomial(3, 0) == 1)
+    assert(spleen_bc.binomial(3, 1) == 3)
+    assert(spleen_bc.binomial(3, 2) == 3)
+    assert(spleen_bc.binomial(3, 3) == 1)
 
-    assert(spleen_bezier_curve.binomial(4, 0) == 1)
-    assert(spleen_bezier_curve.binomial(4, 1) == 4)
-    assert(spleen_bezier_curve.binomial(4, 2) == 6)
-    assert(spleen_bezier_curve.binomial(4, 3) == 4)
-    assert(spleen_bezier_curve.binomial(4, 4) == 1)
+    assert(spleen_bc.binomial(4, 0) == 1)
+    assert(spleen_bc.binomial(4, 1) == 4)
+    assert(spleen_bc.binomial(4, 2) == 6)
+    assert(spleen_bc.binomial(4, 3) == 4)
+    assert(spleen_bc.binomial(4, 4) == 1)
 
-    assert(spleen_bezier_curve.binomial(5, 0) == 1)
-    assert(spleen_bezier_curve.binomial(5, 1) == 5)
-    assert(spleen_bezier_curve.binomial(5, 2) == 10)
-    assert(spleen_bezier_curve.binomial(5, 3) == 10)
-    assert(spleen_bezier_curve.binomial(5, 4) == 5)
-    assert(spleen_bezier_curve.binomial(5, 5) == 1)
+    assert(spleen_bc.binomial(5, 0) == 1)
+    assert(spleen_bc.binomial(5, 1) == 5)
+    assert(spleen_bc.binomial(5, 2) == 10)
+    assert(spleen_bc.binomial(5, 3) == 10)
+    assert(spleen_bc.binomial(5, 4) == 5)
+    assert(spleen_bc.binomial(5, 5) == 1)
 
-    assert(spleen_bezier_curve.binomial(6, 0) == 1)
-    assert(spleen_bezier_curve.binomial(6, 1) == 6)
-    assert(spleen_bezier_curve.binomial(6, 2) == 15)
-    assert(spleen_bezier_curve.binomial(6, 3) == 20)
-    assert(spleen_bezier_curve.binomial(6, 4) == 15)
-    assert(spleen_bezier_curve.binomial(6, 5) == 6)
-    assert(spleen_bezier_curve.binomial(6, 6) == 1)
+    assert(spleen_bc.binomial(6, 0) == 1)
+    assert(spleen_bc.binomial(6, 1) == 6)
+    assert(spleen_bc.binomial(6, 2) == 15)
+    assert(spleen_bc.binomial(6, 3) == 20)
+    assert(spleen_bc.binomial(6, 4) == 15)
+    assert(spleen_bc.binomial(6, 5) == 6)
+    assert(spleen_bc.binomial(6, 6) == 1)
 
-    assert(spleen_bezier_curve.binomial(7, 0) == 1)
-    assert(spleen_bezier_curve.binomial(7, 1) == 7)
-    assert(spleen_bezier_curve.binomial(7, 2) == 21)
-    assert(spleen_bezier_curve.binomial(7, 3) == 35)
-    assert(spleen_bezier_curve.binomial(7, 4) == 35)
-    assert(spleen_bezier_curve.binomial(7, 5) == 21)
-    assert(spleen_bezier_curve.binomial(7, 6) == 7)
-    assert(spleen_bezier_curve.binomial(7, 7) == 1)
+    assert(spleen_bc.binomial(7, 0) == 1)
+    assert(spleen_bc.binomial(7, 1) == 7)
+    assert(spleen_bc.binomial(7, 2) == 21)
+    assert(spleen_bc.binomial(7, 3) == 35)
+    assert(spleen_bc.binomial(7, 4) == 35)
+    assert(spleen_bc.binomial(7, 5) == 21)
+    assert(spleen_bc.binomial(7, 6) == 7)
+    assert(spleen_bc.binomial(7, 7) == 1)
 
-    assert(spleen_bezier_curve.binomial(8, 0) == 1)
-    assert(spleen_bezier_curve.binomial(8, 1) == 8)
-    assert(spleen_bezier_curve.binomial(8, 2) == 28)
-    assert(spleen_bezier_curve.binomial(8, 3) == 56)
-    assert(spleen_bezier_curve.binomial(8, 4) == 70)
-    assert(spleen_bezier_curve.binomial(8, 5) == 56)
-    assert(spleen_bezier_curve.binomial(8, 6) == 28)
-    assert(spleen_bezier_curve.binomial(8, 7) == 8)
-    assert(spleen_bezier_curve.binomial(8, 8) == 1)
+    assert(spleen_bc.binomial(8, 0) == 1)
+    assert(spleen_bc.binomial(8, 1) == 8)
+    assert(spleen_bc.binomial(8, 2) == 28)
+    assert(spleen_bc.binomial(8, 3) == 56)
+    assert(spleen_bc.binomial(8, 4) == 70)
+    assert(spleen_bc.binomial(8, 5) == 56)
+    assert(spleen_bc.binomial(8, 6) == 28)
+    assert(spleen_bc.binomial(8, 7) == 8)
+    assert(spleen_bc.binomial(8, 8) == 1)
 
-    assert(spleen_bezier_curve.binomial(10, 10) == 1)
-    assert(spleen_bezier_curve.binomial(10, 5) == 252)
+    assert(spleen_bc.binomial(10, 10) == 1)
+    assert(spleen_bc.binomial(10, 5) == 252)
 
 
 def test_bernstein():
 
     # for n=0, B_0^0(t) = 1
-    assert(spleen_bezier_curve.bernstein(0, 0,    0) == 1)
-    assert(spleen_bezier_curve.bernstein(0, 0, 0.25) == 1)
-    assert(spleen_bezier_curve.bernstein(0, 0, 0.75) == 1)
-    assert(spleen_bezier_curve.bernstein(0, 0,    1) == 1)
+    assert(spleen_bc.bernstein(0, 0,    0) == 1)
+    assert(spleen_bc.bernstein(0, 0, 0.25) == 1)
+    assert(spleen_bc.bernstein(0, 0, 0.75) == 1)
+    assert(spleen_bc.bernstein(0, 0,    1) == 1)
 
     # for n=1, B_0^1(t) = 1-t
-    assert(spleen_bezier_curve.bernstein(0, 1,    0) == 1)
-    assert(spleen_bezier_curve.bernstein(0, 1, 0.25) == 0.75)
-    assert(spleen_bezier_curve.bernstein(0, 1, 0.75) == 0.25)
-    assert(spleen_bezier_curve.bernstein(0, 1,    1) == 0)
+    assert(spleen_bc.bernstein(0, 1,    0) == 1)
+    assert(spleen_bc.bernstein(0, 1, 0.25) == 0.75)
+    assert(spleen_bc.bernstein(0, 1, 0.75) == 0.25)
+    assert(spleen_bc.bernstein(0, 1,    1) == 0)
 
     # for n=1, B_1^1(t) = t
-    assert(spleen_bezier_curve.bernstein(1, 1,    0) == 0)
-    assert(spleen_bezier_curve.bernstein(1, 1, 0.25) == 0.25)
-    assert(spleen_bezier_curve.bernstein(1, 1, 0.75) == 0.75)
-    assert(spleen_bezier_curve.bernstein(1, 1,    1) == 1)
+    assert(spleen_bc.bernstein(1, 1,    0) == 0)
+    assert(spleen_bc.bernstein(1, 1, 0.25) == 0.25)
+    assert(spleen_bc.bernstein(1, 1, 0.75) == 0.75)
+    assert(spleen_bc.bernstein(1, 1,    1) == 1)
+
+
+def test_bezier_curve_subdiviser():
+
+    ctrl_pts = [(0,0,0), (0.4,-0.1,0.4), (0.6,1.1,0.4), (1,1,0)]
+    t0 = 0.5
+    ctrl_pts_1, ctrl_pts_2 = spleen_bc.bezier_curve_subdivision(ctrl_pts, t0)
+
+    assert(len(ctrl_pts) == len(ctrl_pts_1))
+    assert(len(ctrl_pts) == len(ctrl_pts_2))
+
+    eps = 10e-9
+
+    t = 0.25
+    assert( abs(spleen_bc.bezier_curve(ctrl_pts, t*t0)[0] - spleen_bc.bezier_curve(ctrl_pts_1, t)[0]) < eps)
+    assert( abs(spleen_bc.bezier_curve(ctrl_pts, t*t0)[1] - spleen_bc.bezier_curve(ctrl_pts_1, t)[1]) < eps)
+    assert( abs(spleen_bc.bezier_curve(ctrl_pts, t*t0)[2] - spleen_bc.bezier_curve(ctrl_pts_1, t)[2]) < eps)
+
+    t = 0.6
+    assert( abs(spleen_bc.bezier_curve(ctrl_pts, t*t0)[0] - spleen_bc.bezier_curve(ctrl_pts_1, t)[0]) < eps)
+    assert( abs(spleen_bc.bezier_curve(ctrl_pts, t*t0)[1] - spleen_bc.bezier_curve(ctrl_pts_1, t)[1]) < eps)
+    assert( abs(spleen_bc.bezier_curve(ctrl_pts, t*t0)[2] - spleen_bc.bezier_curve(ctrl_pts_1, t)[2]) < eps)
+
+    t = 0.83
+    assert( abs(spleen_bc.bezier_curve(ctrl_pts, t*t0)[0] - spleen_bc.bezier_curve(ctrl_pts_1, t)[0]) < eps)
+    assert( abs(spleen_bc.bezier_curve(ctrl_pts, t*t0)[1] - spleen_bc.bezier_curve(ctrl_pts_1, t)[1]) < eps)
+    assert( abs(spleen_bc.bezier_curve(ctrl_pts, t*t0)[2] - spleen_bc.bezier_curve(ctrl_pts_1, t)[2]) < eps)
+
+    t = 0.12
+    assert( abs(spleen_bc.bezier_curve(ctrl_pts, t0+(1-t0)*t)[0] - spleen_bc.bezier_curve(ctrl_pts_2, t)[0]) < eps)
+    assert( abs(spleen_bc.bezier_curve(ctrl_pts, t0+(1-t0)*t)[1] - spleen_bc.bezier_curve(ctrl_pts_2, t)[1]) < eps)
+    assert( abs(spleen_bc.bezier_curve(ctrl_pts, t0+(1-t0)*t)[2] - spleen_bc.bezier_curve(ctrl_pts_2, t)[2]) < eps)
+
+    t = 0.49
+    assert( abs(spleen_bc.bezier_curve(ctrl_pts, t0+(1-t0)*t)[0] - spleen_bc.bezier_curve(ctrl_pts_2, t)[0]) < eps)
+    assert( abs(spleen_bc.bezier_curve(ctrl_pts, t0+(1-t0)*t)[1] - spleen_bc.bezier_curve(ctrl_pts_2, t)[1]) < eps)
+    assert( abs(spleen_bc.bezier_curve(ctrl_pts, t0+(1-t0)*t)[2] - spleen_bc.bezier_curve(ctrl_pts_2, t)[2]) < eps)
+
+    t = 0.97
+    assert( abs(spleen_bc.bezier_curve(ctrl_pts, t0+(1-t0)*t)[0] - spleen_bc.bezier_curve(ctrl_pts_2, t)[0]) < eps)
+    assert( abs(spleen_bc.bezier_curve(ctrl_pts, t0+(1-t0)*t)[1] - spleen_bc.bezier_curve(ctrl_pts_2, t)[1]) < eps)
+    assert( abs(spleen_bc.bezier_curve(ctrl_pts, t0+(1-t0)*t)[2] - spleen_bc.bezier_curve(ctrl_pts_2, t)[2]) < eps)
